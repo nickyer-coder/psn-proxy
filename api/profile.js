a0c50af6d674311e48c68bbecdba28cc603e0eec
nickyer-coder <colle.david.1418@gmail.com>
2026-07-30 14:08:45 +0200

    Update profile.js

--- api/profile.js
+++ api/profile.js
@@ -1,7 +1,6 @@
 import { 
   exchangeNpssoForCode, 
   exchangeCodeForAccessToken, 
-  getUserRegister,
   getProfileFromAccountId 
 } from "psn-api";
 
@@ -17,11 +16,12 @@ export default async function handler(req, res) {
     const accessCode = await exchangeNpssoForCode(npsso);
     const authorization = await exchangeCodeForAccessToken(accessCode);
 
-    // 2. Si aucun accountId fourni, on récupère l'accountId du compte connecté
+    // 2. Récupérer l'accountId : soit passé en paramètre, soit extrait directement du token
     let accountId = req.query.accountId;
+    
     if (!accountId || accountId === "me") {
-      const userInfo = await getUserRegister(authorization);
-      accountId = userInfo.accountId;
+      // authorization contient directement l'accountId du jeton connecté !
+      accountId = authorization.accountId; 
     }
 
     // 3. Récupération du profil
